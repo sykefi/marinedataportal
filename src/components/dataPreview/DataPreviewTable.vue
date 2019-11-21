66412d73f7c0dc90d6289698e96c66c69f383617
Simo Paasisalo <simo.paasisalo@ymparisto.fi>
2019-11-21 10:01:53 +0000

    Merged PR 236: fix: #933 bug in file download
    fix: #933 bug in file download
    
    Related work items: #933

--- src/components/dataPreview/DataPreviewTable.vue
+++ src/components/dataPreview/DataPreviewTable.vue
@@ -1,64 +1,56 @@
 <template>
   <div>
     <div v-if="hasData">
-      <strong>{{$t(module.name)}}</strong>
-      <span>{{` (${module.rowCount} ${$t('$rows')})`}}</span>
-      <button @click="downloadCsv">{{$t('$downloadCSV')}}</button>
+      <strong>{{ $t(module.name) }}</strong>
+      <span>{{ ` (${module.rowCount} ${$t("$rows")}) ` }}</span>
+      <a :href="encodedFileUri" :download="$t(module.name) + '.csv'">{{
+        $t("$downloadCSV")
+      }}</a>
       <table>
         <tr>
-          <th v-for="(name,index) in columnNames" :key="index">{{name}}</th>
+          <th v-for="(name, index) in columnNames" :key="index">{{ name }}</th>
         </tr>
         <tr v-for="(row, index) in module.previewData" :key="index">
-          <td v-for="(column,index) of row" :key="index">{{column}}</td>
+          <td v-for="(column, index) of row" :key="index">{{ column }}</td>
         </tr>
       </table>
     </div>
     <div v-if="!hasData && isDataLoaded">
-      <strong>{{$t(module.name)}}</strong>
-      - {{$t('$noRowsFound')}}
+      <strong>{{ $t(module.name) }}</strong>
+      - {{ $t("$noRowsFound") }}
     </div>
   </div>
 </template>
 
-<script lang='ts'>
-  import { Component, Prop, Vue } from 'vue-property-decorator';
-  import { IAttributeModule } from '@/store/attributeModules/IAttributeModule';
-  import i18n from '@/locale/i18n';
-  @Component
-  export default class DataPreviewTable extends Vue {
-    @Prop({ required: true })
-    public module!: IAttributeModule;
+<script lang="ts">
+import { Component, Prop, Vue } from 'vue-property-decorator';
+import { IAttributeModule } from '@/store/attributeModules/IAttributeModule';
+@Component
+export default class DataPreviewTable extends Vue {
+  @Prop({ required: true })
+  public module!: IAttributeModule;
 
-    get isDataLoaded() {
-      return this.module.data !== null;
-    }
-
-    get hasData() {
-      return this.module.previewData.length > 0;
-    }
-
-    get columnNames() {
-      return Object.keys(this.module.previewData[0]);
-    }
-
-    public downloadCsv() {
-      let csvContent = 'data:text/csv;charset=utf-8,';
-      csvContent += this.columnNames.join(';') + ';\r\n';
-
-      this.module.data!.forEach((row) => {
-        const values = Object.keys(row).map((key) => (row as any)[key]);
-        csvContent += values.join(';') + ';\r\n';
-      });
-      const encodedUri = encodeURI(csvContent);
-      const link = document.createElement('a');
-      link.setAttribute('href', encodedUri);
-      link.setAttribute('download', `${i18n.t(this.module.name)}.csv`);
-
-      link.click(); // This will download the data with the module name.
-    }
-
+  get isDataLoaded() {
+    return this.module.data !== null;
   }
-</script>
 
-<style lang='scss' scoped>
-</style>
+  get hasData() {
+    return this.module.previewData.length > 0;
+  }
+
+  get columnNames() {
+    return Object.keys(this.module.previewData[0]);
+  }
+
+  get encodedFileUri() {
+    let csvContent = 'data:text/csv;charset=utf-8,\uFEFF';
+    csvContent += this.columnNames.join(';') + ';\r\n';
+
+    this.module.data!.forEach((row) => {
+      const values = Object.keys(row).map((key) => (row as any)[key]);
+      csvContent += values.join(';') + ';\r\n';
+    });
+    return encodeURI(csvContent);
+  }
+}
+</script>
