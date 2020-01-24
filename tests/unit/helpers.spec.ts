c8801bf81a8d19a3a8443c10eb561e7d10ddaffc
Simo Paasisalo <Simo.Paasisalo@env.fi>
2020-01-24 16:01:50 +0200

    fix lint

--- tests/unit/helpers.spec.ts
+++ tests/unit/helpers.spec.ts
@@ -1,4 +1,5 @@
 // tslint:disable:no-unused-expression
+// tslint:disable-next-line: max-line-length
 import {
     getTimeParametersForVeslaFilter, isDateInPeriod, validateSearchParameters,
     toFmiFormat, toCommonFormat, fromObservationToSykeFormat, fromWaterQualityResultToSykeFormat,
