07c4cfcc1a42cd7c32c4c1ed085bd53fa4a9a06e
Simo Paasisalo <github@simopaasisalo.fastmail.com>
2020-02-03 09:30:59 +0200

    build: remove vue-router (#31)

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,5 @@
 import Vue from 'vue';
 import App from './App.vue';
-import router from './router';
 import store from './store/store';
 import i18n from '@/locale/i18n';
 import VueLayers from 'vuelayers';
@@ -31,7 +30,6 @@ new Vue({
     const html = document.documentElement;
     html.setAttribute('lang', 'fi');
   },
-  router,
   store,
   i18n,
   render: (h) => h(App),
