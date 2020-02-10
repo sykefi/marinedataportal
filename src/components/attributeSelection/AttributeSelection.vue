44ac66915985a9242afb6e2d06901c3756fe3344
Vanamo Piirainen <vanamo.piirainen@ymparisto.fi>
2020-02-10 17:24:10 +0200

    Remove disabled attribute buttons

--- src/components/attributeSelection/AttributeSelection.vue
+++ src/components/attributeSelection/AttributeSelection.vue
@@ -2,25 +2,38 @@
   <div>
     <SelectionHeader :header="$t('$attributeSelectionTitle')" />
     <p class="error" v-if="showAttributeError">{{$t(attributeErrorMessage)}}</p>
-    <div class="selections-grid">
-      <SelectionButton name="$secchiDepth" :module="secchiDepthModule" :disabled="!sykeApiOnline" />
-      <SelectionButton name="$waterLevel" :module="waterLevelModule" :disabled="!fmiApiOnline" />
-      <SelectionButton name="$iceThickness" :module="iceThicknessModule" :disabled="!sykeApiOnline" />
-      <SelectionButton name="$surge" :module="surgeModule" :expandable="true" :disabled="!fmiApiOnline" />
-      <SelectionButton
-        name="$surfaceTemperature"
-        :module="surfaceTemperatureModule"
-        :expandable="true"
-        :disabled="!sykeApiOnline && !fmiApiOnline"
-      />
-      <SelectionButton
-        name="$waterQuality"
-        :module="waterQualityModule"
-        :expandable="true"
-        :disabled="!sykeApiOnline"
-      />
-      <SelectionButton name="$phytoplankton" :module="phytoplanktonModule" :disabled="true" />
-      <SelectionButton name="$benthicFauna" :module="benthicFaunaModule" :disabled="true" />
+    <div id="button-wrapper">
+      <div class="selections-grid">
+        <SelectionButton
+          name="$secchiDepth"
+          :module="secchiDepthModule"
+          :disabled="!sykeApiOnline"
+        />
+        <SelectionButton name="$waterLevel" :module="waterLevelModule" :disabled="!fmiApiOnline" />
+        <SelectionButton
+          name="$iceThickness"
+          :module="iceThicknessModule"
+          :disabled="!sykeApiOnline"
+        />
+        <SelectionButton
+          name="$surge"
+          :module="surgeModule"
+          :expandable="true"
+          :disabled="!fmiApiOnline"
+        />
+        <SelectionButton
+          name="$surfaceTemperature"
+          :module="surfaceTemperatureModule"
+          :expandable="true"
+          :disabled="!sykeApiOnline && !fmiApiOnline"
+        />
+        <SelectionButton
+          name="$waterQuality"
+          :module="waterQualityModule"
+          :expandable="true"
+          :disabled="!sykeApiOnline"
+        />
+      </div>
     </div>
     <div class="detail-box-container">
       <SurgeDetails v-if="surgeModule.isSelected" />
@@ -34,10 +47,15 @@
 
 <style lang="scss">
 @import "@/assets/styles/variables.scss";
-.selections-grid {
+#button-wrapper {
   display: grid;
-  grid-template-columns: 25% 25% 25% 25%;
-  grid-template-rows: auto;
+  grid-template:
+    ". but but but ."
+}
+.selections-grid {
+  grid-area: but;
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr;
   grid-row-gap: 2rem;
 }
 .detail-box-container {
