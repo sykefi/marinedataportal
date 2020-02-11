67960b410fae1f2ab41546d79662377075a63ec3
Simo Paasisalo <simo.paasisalo@env.fi>
2020-02-11 09:49:01 +0200

    cleanup

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -66,80 +66,80 @@
 </template>
 
 <script lang="ts">
-  import { Component, Vue } from 'vue-property-decorator';
-  import i18n from '@/locale/i18n';
+import { Component, Vue } from 'vue-property-decorator';
+import i18n from '@/locale/i18n';
 
-  @Component({ i18n })
-  export default class Footer extends Vue { }
+@Component({ i18n })
+export default class Footer extends Vue { }
 </script>
 
 <style lang="scss" scoped>
-  @import "@/assets/styles/variables.scss";
-  #app-footer {
-    display: grid;
-    grid-template:
-      "div div div"
-      ". logo ."
-      ". links ."
-      / 1fr $content-width 1fr;
-  }
+@import "@/assets/styles/variables.scss";
+#app-footer {
+  display: grid;
+  grid-template:
+    "div div div"
+    ". logo ."
+    ". links ."
+    / 1fr $content-width 1fr;
+}
 
-  #feedback {
-    grid-area: div;
-    height: 4.4rem;
-    background-color: $border-dark;
-    margin-top: 5rem;
-    margin-bottom: 1rem;
+#feedback {
+  grid-area: div;
+  height: 4.4rem;
+  background-color: $border-dark;
+  margin-top: 5rem;
+  margin-bottom: 1rem;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  a {
+    color: $text-white;
+    font-weight: bold;
+    font-size: $font-size-l;
+    letter-spacing: 0.05rem;
+    text-decoration: none;
+  }
+}
+
+#arrow-icon {
+  color: $background-bright-blue;
+  font-size: $font-size-xl;
+  padding-right: 0.5rem;
+}
+
+#logos {
+  grid-area: logo;
+  ul {
     display: flex;
-    align-items: center;
+    align-items: baseline;
     justify-content: center;
-    a {
-      color: $text-white;
-      font-weight: bold;
-      font-size: $font-size-l;
-      letter-spacing: 0.05rem;
-      text-decoration: none;
+    height: 3rem;
+    list-style-type: none;
+    width: 100%;
+    padding: 0;
+    li {
+      height: 100%;
+      padding: 2rem;
     }
   }
 
-  #arrow-icon {
-    color: $background-bright-blue;
-    font-size: $font-size-xl;
-    padding-right: 0.5rem;
+  #syke-logo {
+    width: 4rem;
+    height: auto;
   }
 
-  #logos {
-    grid-area: logo;
-    ul {
-      display: flex;
-      align-items: baseline;
-      justify-content: center;
-      height: 3rem;
-      list-style-type: none;
-      width: 100%;
-      padding: 0;
-      li {
-        height: 100%;
-        padding: 2rem;
-      }
-    }
-
-    #syke-logo {
-      width: 4rem;
-      height: auto;
-    }
-
-    #il-logo {
-      width: 15rem;
-      height: auto;
-    }
+  #il-logo {
+    width: 15rem;
+    height: auto;
   }
+}
 
-  #links {
-    grid-area: links;
-    ul {
-      list-style: none;
-      text-align: right;
-    }
+#links {
+  grid-area: links;
+  ul {
+    list-style: none;
+    text-align: right;
   }
+}
 </style>
