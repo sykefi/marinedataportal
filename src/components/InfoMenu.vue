9b88febc2ce2073ae5bf18443210e15a2044df47
Simo Paasisalo <simo.paasisalo@env.fi>
2020-02-11 08:40:19 +0200

    cleanup?

--- src/components/InfoMenu.vue
+++ src/components/InfoMenu.vue
@@ -1,103 +1,90 @@
 <template>
   <div>
     <div id="app-info">
-      <div class="common fill1" />
-      <div class="common info">
+      <div id="buttons">
         <InfoButton :buttonText="$t('$serviceInfoTitle')" @show-info="onShowInfo" />
         <InfoButton :buttonText="$t('$instructionsTitle')" @show-info="onShowInstructions" />
         <InfoButton :buttonText="$t('$termsOfServiceTitle')" @show-info="onShowTerms" />
       </div>
-      <div class="common fill2" />
-      <div id="info-text">
-        <div v-if="showInfo">
-          <h2>{{$t('$serviceInfoTitle')}}</h2>
-          <p>{{$t('$serviceInfo')}}</p>
-        </div>
-        <div v-if="showInstructions">
-          <h2>{{$t('$instructionsTitle')}}</h2>
-          <p>{{$t('$instructions')}}</p>
-        </div>
-        <div v-if="showTerms">
-          <h2>{{$t('$termsOfServiceTitle')}}</h2>
-          <p>{{$t('$termsOfService')}}</p>
-        </div>
+    </div>
+    <div id="info-text">
+      <div v-if="showInfo">
+        <h2>{{$t('$serviceInfoTitle')}}</h2>
+        <p>{{$t('$serviceInfo')}}</p>
+      </div>
+      <div v-if="showInstructions">
+        <h2>{{$t('$instructionsTitle')}}</h2>
+        <p>{{$t('$instructions')}}</p>
+      </div>
+      <div v-if="showTerms">
+        <h2>{{$t('$termsOfServiceTitle')}}</h2>
+        <p>{{$t('$termsOfService')}}</p>
       </div>
     </div>
   </div>
 </template>
 
 <script lang="ts">
-import { Component, Vue } from 'vue-property-decorator';
-import InfoButton from '@/components/InfoButton.vue';
+  import { Component, Vue } from 'vue-property-decorator';
+  import InfoButton from '@/components/InfoButton.vue';
 
-@Component({
-  components: {
-    InfoButton,
-  },
-})
-export default class InfoMenu extends Vue {
-  public showInfo: boolean = false;
-  public showInstructions: boolean = false;
-  public showTerms: boolean = false;
+  @Component({
+    components: {
+      InfoButton,
+    },
+  })
+  export default class InfoMenu extends Vue {
+    public showInfo: boolean = false;
+    public showInstructions: boolean = false;
+    public showTerms: boolean = false;
 
-  public onShowInfo(isPressed: boolean) {
-    this.showInfo = isPressed;
+    public onShowInfo(isPressed: boolean) {
+      this.showInfo = isPressed;
+    }
+
+    public onShowInstructions(isPressed: boolean) {
+      this.showInstructions = isPressed;
+    }
+
+    public onShowTerms(isPressed: boolean) {
+      this.showTerms = isPressed;
+    }
   }
-
-  public onShowInstructions(isPressed: boolean) {
-    this.showInstructions = isPressed;
-  }
-
-  public onShowTerms(isPressed: boolean) {
-    this.showTerms = isPressed;
-  }
-}
 </script>
 
 <style lang="scss" scoped>
-@import "@/assets/styles/variables.scss";
-#app-info {
-  display: grid;
-  grid-template:
-    "fill1 div fill2" 4.4rem
-    ". info ."
-    / 1fr 65rem 1fr;
-}
+  @import "@/assets/styles/variables.scss";
+  #app-info {
+    display: grid;
+    grid-template:
+      ". buttons ." 4.4rem
+      ". info ."
+      / 1fr 65rem 1fr;
+    border-top: 0.05rem solid black;
+    background-color: $border-dark;
+    border-bottom: 0.2rem solid $border-red;
 
-.common {
-  border-top: 0.05rem solid black;
-  border-bottom: 0.2rem solid $border-red;
-  background-color: $border-dark;
-}
-
-.fill1 {
-  grid-area: fill1;
-}
-
-.fill2 {
-  grid-area: fill2;
-}
-
-.info {
-  grid-area: div;
-  display: flex;
-  align-items: center;
-  justify-content: space-evenly;
-}
-
-#info-text {
-  grid-area: info;
-  text-align: left;
-  padding: 0 4rem 0 4rem;
-  margin-top: 2rem;
-  h2 {
-    padding-top: 1rem;
-    font-size: $font-size-l;
-    font-weight: bold;
-    letter-spacing: 0.05rem;
+    #buttons {
+      grid-area: buttons;
+      display: flex;
+      align-items: center;
+      justify-content: space-evenly;
+    }
   }
-  p {
-    font-size: $font-size-m;
+
+  #info-text {
+    grid-area: info;
+    text-align: left;
+    padding: 0 4rem 0 4rem;
+    margin-top: 2rem;
+    h2 {
+      padding-top: 1rem;
+      font-size: $font-size-l;
+      font-weight: bold;
+      letter-spacing: 0.05rem;
+    }
+    p {
+      font-size: $font-size-m;
+    }
   }
-}
 </style>
