1332e05363ba587a66c46e28e2f725378dbcead2
Piirainen Vanamo <Vanamo.Piirainen@env.fi>
2020-03-17 11:07:57 +0200

    Move funding logos inline with language options

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,52 +1,50 @@
 <template>
   <header>
-    <div id="top-header">
+    <div id="upper-header">
       <!-- Finnish -->
-      <div id="top-logos" v-show="$i18n.locale === 'fi'">
-        <img src="@/assets/logos/EU_fi_en.jpg" alt="EU logo." />
-        <img
-          src="@/assets/logos/emkr_fi.png"
-          alt="Logo Euroopan meri- ja kalatalousrahasto, Suomen toimintaohjelma 2014-2020."
-        />
+      <div id="project-logo" v-if="$i18n.locale === 'fi'">
+        <a href="http://itameri.fi">
+          <img src="@/assets/logos/Hankelogo_fi_en.png" alt="Itämeri.fi kotisivu." />
+        </a>
       </div>
 
       <!-- English -->
-      <div id="top-logos" v-show="$i18n.locale === 'en'">
-        <img src="@/assets/logos/EU_fi_en.jpg" alt="EU logo" />
-        <img
-          src="@/assets/logos/emkr_eng.jpg"
-          alt="Logo of the European maritime and fisheries fund, operational programme for Finland 2014-2020."
-        />
+      <div id="project-logo" v-if="$i18n.locale === 'en'">
+        <a href="http://itameri.fi">
+          <img src="@/assets/logos/Hankelogo_fi_en.png" alt="Itämeri.fi home." />
+        </a>
       </div>
 
-      <div id="upper-header">
+      <div id="lang-and-logos">
+        <a
+          href="#"
+          :class="{current: $i18n.locale === 'fi'}"
+          @click="setLanguage('fi')"
+          aria-label="Vaihda kieleksi suomi."
+        >FI</a>
+        <a
+          href="#"
+          :class="{current: $i18n.locale === 'en'}"
+          @click="setLanguage('en')"
+          aria-label="Change language to English."
+        >EN</a>
+
         <!-- Finnish -->
-        <div class="header-logos" v-if="$i18n.locale === 'fi'">
-          <a href="http://itameri.fi">
-            <img src="@/assets/logos/Hankelogo_fi_en.png" alt="Itämeri.fi kotisivu." />
-          </a>
+        <div id="fund-logos" v-show="$i18n.locale === 'fi'">
+          <img
+            src="@/assets/logos/emkr_fi.png"
+            alt="Logo Euroopan meri- ja kalatalousrahasto, Suomen toimintaohjelma 2014-2020."
+          />
+          <img src="@/assets/logos/EU_fi_en.jpg" alt="EU logo." />
         </div>
 
         <!-- English -->
-        <div class="header-logos" v-if="$i18n.locale === 'en'">
-          <a href="http://itameri.fi">
-            <img src="@/assets/logos/Hankelogo_fi_en.png" alt="Itämeri.fi home." />
-          </a>
-        </div>
-
-        <div id="lang">
-          <a
-            href="#"
-            :class="{current: $i18n.locale === 'fi'}"
-            @click="setLanguage('fi')"
-            aria-label="Vaihda kieleksi suomi."
-          >FI</a>
-          <a
-            href="#"
-            :class="{current: $i18n.locale === 'en'}"
-            @click="setLanguage('en')"
-            aria-label="Change language to English."
-          >EN</a>
+        <div id="fund-logos" v-show="$i18n.locale === 'en'">
+          <img
+            src="@/assets/logos/emkr_eng.jpg"
+            alt="Logo of the European maritime and fisheries fund, operational programme for Finland 2014-2020."
+          />
+          <img src="@/assets/logos/EU_fi_en.jpg" alt="EU logo" />
         </div>
       </div>
     </div>
@@ -54,11 +52,11 @@
 </template>
 
 <script lang="ts">
-import { Component, Vue } from 'vue-property-decorator';
-import i18n from '@/locale/i18n';
-import { waterQualityModule } from '@/store/attributeModules/waterQualityModule';
-import { surfaceTemperatureModule } from '@/store/attributeModules/surfaceTemperatureModule';
-import { surgeModule } from '@/store/attributeModules/surgeModule';
+import { Component, Vue } from "vue-property-decorator";
+import i18n from "@/locale/i18n";
+import { waterQualityModule } from "@/store/attributeModules/waterQualityModule";
+import { surfaceTemperatureModule } from "@/store/attributeModules/surfaceTemperatureModule";
+import { surgeModule } from "@/store/attributeModules/surgeModule";
 
 @Component({ i18n })
 export default class Header extends Vue {
@@ -76,27 +74,19 @@ export default class Header extends Vue {
 
 <style lang="scss" scoped>
 @import "@/assets/styles/variables.scss";
-#top-header {
-  display: grid;
-  grid-template:
-    "fund fund fund"
-    ". lang ."
-    / auto $content-width auto;
-}
 
 #upper-header {
-  grid-area: lang;
   display: flex;
   justify-content: space-between;
   align-items: center;
   height: 6.25rem;
+  padding: 0 2rem 0 6rem;
 }
 
-#lang {
+#lang-and-logos {
   display: flex;
   justify-content: space-evenly;
   align-items: center;
-  width: 5rem;
   height: 30%;
   a {
     padding: 0 0.5rem 0 0.5rem;
@@ -115,23 +105,16 @@ a {
   }
 }
 
-.header-logos {
+#project-logo {
   height: 45%;
-  a {
-    padding-right: 4rem;
-  }
 }
 
-#top-logos {
-  grid-area: fund;
-  height: 3.5rem;
-  background-color: $background-light;
+#fund-logos {
   display: flex;
-  justify-content: flex-end;
-  align-items: center;
+  padding-left: 2rem;
   img {
     margin: 0 1rem 0 1rem;
-    height: 70%;
+    height: 3rem;
     vertical-align: bottom;
   }
 }
