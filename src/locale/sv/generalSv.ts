c6fe9fdd1bd776f082280f17e8394255a83e1540
Piirainen Vanamo <Vanamo.Piirainen@env.fi>
2020-03-31 13:20:48 +0300

    Modify Swedish titles

--- src/locale/sv/generalSv.ts
+++ src/locale/sv/generalSv.ts
@@ -1,6 +1,6 @@
 export default {
-    $siteTitle: 'Ladda ner uppgifter om tillståndet i Östersjön',
-    $siteDetails: 'Här kan du ladda ner data om övervakning av havets tillstånd från Finlands miljöcentral och Meteorologiska institutet',
+    $siteTitle: 'Ladda ner data med information om Östersjöns tillstånd',
+    $siteDetails: 'Observationer om havets tillstånd och förhållanden vid Finlands miljöcentral och Meteorologiska institutet',
     $missingTimeSpanStart: 'Startdatum för tidsintervall saknas.',
     $missingTimeSpanEnd: 'Slutdatum för tidsintervall saknas.',
     $noAttributesSelected: 'Välj minst en sökparameter.',
