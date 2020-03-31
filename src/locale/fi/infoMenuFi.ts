5e7eef79dc2806b26f02d8f005283ac3b5c906a4
Piirainen Vanamo <Vanamo.Piirainen@env.fi>
2020-03-31 13:27:23 +0300

    Remove lorem ipsum

--- src/locale/fi/infoMenuFi.ts
+++ src/locale/fi/infoMenuFi.ts
@@ -1,6 +1,6 @@
 export default {
     $serviceInfoTitle: 'Tietoa palvelusta',
-    $serviceInfo: 'Lorem ipsum dolor sit amet, duo cu commodo persius, eum cu reque facer. Natum illud eu eos, aliquid reprehendunt ut eum, placerat signiferumque est ea. Illud illum ut sit, et wisi libris eum. An accusam gubergren vulputate sea. Mea case choro in, qui ne laudem graeci.',
+    $serviceInfo: 'Tietoa palvelusta',
     $instructionsTitle: 'Käyttöohje',
     $instructions: 'Käyttöohje',
     $termsOfServiceTitle: 'Käyttöehdot',
