b383ba1d4aa0a72f58f2d0c7a04d356d6681ef96
Vanamo Piirainen <vanamo.piirainen@env.fi>
2021-08-05 14:29:25 +0300

    infra: Update packages, replace Node Sass with Dart Sass (#155)
    * Update packages, remove node-sass, add sass
    
    * Fix issue with dart sass and unit testing
    
    * Add comment about dart-sass issue workaround
    
    Co-authored-by: Piirainen Vanamo <Vanamo.Piirainen@syke.fi>

--- vue.config.js
+++ vue.config.js
@@ -4,5 +4,15 @@ module.exports = {
     port: 8080,
     public: "0.0.0.0:8080"
   },
-  publicPath: "./"
+  publicPath: "./",
+  // https://github.com/vuejs/vue-cli/issues/4053#issuecomment-544641072
+  chainWebpack: (config) => {
+    if (process.env.NODE_ENV === 'test') {
+      const scssRule = config.module.rule('scss');
+      scssRule.uses.clear();
+      scssRule
+        .use('null-loader')
+        .loader('null-loader');
+    }
+  },
 };
