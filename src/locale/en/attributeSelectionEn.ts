8be0b86196fe21a807d7f0bb36cffc7c8b9064f6
Vanamo Piirainen <vanamo.piirainen@env.fi>
2023-04-27 12:03:46 +0300

    feat: #14632 Change warning message for phosphorus observations (#183)

--- src/locale/en/attributeSelectionEn.ts
+++ src/locale/en/attributeSelectionEn.ts
@@ -10,7 +10,7 @@ export default {
   $benthicFauna: 'Benthic fauna',
   $selectAll: 'Select all',
   $phosphorusMessage:
-    'Anomalies have been observed in phosphorus data of open sea stations from January 1, 2000 to February 28, 2014. The data is under checking and can’t be loaded for the time being.',
+    'Anomalies have been observed in phosphorus data of open sea stations from January 1, 2000 to February 28, 2014, and the data has been removed.',
   $veslaInfo:
     'The information is available in more detail from the <a target="_blank" rel="noopener" href="https://www.syke.fi/fi-FI/Avoin_tieto/Ymparistotietojarjestelmat">Open Information Service</a> of SYKE (in Finnish only).',
 }
